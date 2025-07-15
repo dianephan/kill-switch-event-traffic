07f0219e95e6eabad8bd0f1500a1a23cfb93e48d
diane phan <dianephan@dianes-mbp.mynetworksettings.com>
2025-07-14 21:40:33 -0400

    🔥 comment out some lines in order for starter code to work

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from ldclient.config import Config
 from ldclient.context import Context
 import requests
 from threading import Event
-from halo import Halo
 from character_data import names, character_quotes
 from feed_generator import generate_feed, get_random_avatar
 
@@ -21,23 +20,29 @@ flag_key = "replace-key-here"
 
 @app.route('/')
 def index():
-    # Get feature flag values
-    show_avatar = ldclient.get().variation(flag_key, user_context, False)
+
+    user = "Sandy"
+    show_avatar = False
+    show_recommendations = True
+    avatar_url = "https://media.tenor.com/ocYNcAWYyHMAAAAM/99-cat.gif"
     
-    if show_avatar:
-        avatar_url = get_random_avatar()
-        # Recommendations are only shown if we get a real avatar
-        show_recommendations = True
-    else:
-        avatar_url = "https://media.tenor.com/ocYNcAWYyHMAAAAM/99-cat.gif"
-        show_recommendations = False
+    # # Get feature flag values
+    # show_avatar = ldclient.get().variation(flag_key, user_context, False)
+    
+    # if show_avatar:
+    #     avatar_url = get_random_avatar()
+    #     # Recommendations are only shown if we get a real avatar
+    #     show_recommendations = True
+    # else:
+    #     avatar_url = "https://media.tenor.com/ocYNcAWYyHMAAAAM/99-cat.gif"
+    #     show_recommendations = False
 
     # Generate social media feed
     feed_posts = generate_feed(show_avatar)
 
     return render_template(
         'index.html',
-        user=user_context.to_dict(),
+        user=user,
         avatar_image=avatar_url,
         show_avatar=show_avatar,
         posts=feed_posts,
@@ -50,25 +55,25 @@ def show_evaluation_result(key: str, value: bool):
 
 
 if __name__ == "__main__":
-    if not sdk_key:
-        print("*** Please set the LAUNCHDARKLY_SDK_KEY env first")
-        exit()
+    # if not sdk_key:
+    #     print("*** Please set the LAUNCHDARKLY_SDK_KEY env first")
+    #     exit()
 
-    # Initialize the LaunchDarkly client
-    ldclient.set_config(Config(sdk_key))
+    # # Initialize the LaunchDarkly client
+    # ldclient.set_config(Config(sdk_key))
 
-    if not ldclient.get().is_initialized():
-        print("*** SDK failed to initialize. Please check your internet connection and SDK credential for any typo.")
-        exit()
+    # if not ldclient.get().is_initialized():
+    #     print("*** SDK failed to initialize. Please check your internet connection and SDK credential for any typo.")
+    #     exit()
 
-    print("*** SDK successfully initialized")
-    # Set up the evaluation context. This context should appear on your
-    # LaunchDarkly contexts dashboard soon after you run the demo.
-    user_context = \
-        Context.builder('example-user-key').kind('user').name('Sandy').build()
+    # print("*** SDK successfully initialized")
+    # # Set up the evaluation context. This context should appear on your
+    # # LaunchDarkly contexts dashboard soon after you run the demo.
+    # user_context = \
+    #     Context.builder('example-user-key').kind('user').name('Sandy').build()
 
-    flag_value = ldclient.get().variation(flag_key, user_context, False)
-    show_evaluation_result(flag_key, flag_value)
+    # flag_value = ldclient.get().variation(flag_key, user_context, False)
+    # show_evaluation_result(flag_key, flag_value)
 
     app.run(debug=True)
     
